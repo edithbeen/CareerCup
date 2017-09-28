44846171296301bb4325eb9830755f108742a198
Jiajia Gu <edithbeen@gmail.com>
2017-09-27 23:24:46 -0700

    Q2 and Q3 added

---
+++ Q2_K_Palindrome_String.py
@@ -0,0 +1,5 @@
+# a k-palindrome string is a string which transforms to a palindrome if at most k character is removed
+# given a string s and an integer k, write a function that returns Yes if it is a k-palindrome, otherwise No
+# S has at most 20,000 characters, 0<k<=30
+
+# This is too hard. I don't know the answer now
---
+++ Q3_Num_to_String_Decode.py
@@ -0,0 +1,10 @@
+# set a = 1, b = 2, ..., z = 26
+# given a number, find all possible string it represents
+# eg: input 1123, the output should be aabc, kbc, alc, ...
+# ask how long is the code?
+
+
+def decode(code):
+    code = str(code)
+    # loop from the end
+    for i in
