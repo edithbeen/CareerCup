a4d56984824063d1a0d738ef76533dfeed61080a
jgu <edithbeen@gmail.com>
2017-09-28 12:18:45 -0700

    completed Q6

--- Q6_Multiply_All_Elements_Except_Itself_in_Array.py
+++ Q6_Multiply_All_Elements_Except_Itself_in_Array.py
@@ -1,1 +1,19 @@
-#
+# given a numeric integer as input, return a array with the multiplication of all values in this array except itself
+
+# question: null value and na values?
+from numpy import prod
+
+def multi_others(array):
+    ls = []
+    for i in range(len(array)):
+        value_i = prod(array[:i] + array[i+1:])
+        ls = ls + [value_i]
+    return ls
+
+array = [3, 3, 5, 7]
+print(multi_others(array))
+
+# list expression, easier eay
+ls = [prod(array[:i] + array[i+1:]) for i in range(len(array))]
+
+
