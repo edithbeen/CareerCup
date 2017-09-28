5fb48d573b1cc6e62f88d848ed5f6e52d0856720
jgu <edithbeen@gmail.com>
2017-09-28 15:15:08 -0700

    completed Q7

---
+++ Q8_Array_Repostion_with_Value_in_Position.py
@@ -0,0 +1,9 @@
+# WAP to re-arrange an array and make arr[l] = arr[arr[l]]
+# do in place w/0 using extra memeory
+# the array contains 0 to n-1 integers
+
+array = [1, 3, 2, 4, 0, 5]
+
+# cheating way, list expression, haha
+print([array[x] for x in array])
+
