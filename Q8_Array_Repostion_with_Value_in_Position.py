f66730ff3232bd11e0bd2749f3d4b79e47b9a736
jgu <edithbeen@gmail.com>
2017-09-28 17:14:51 -0700

    completed Q8

--- Q8_Array_Repostion_with_Value_in_Position.py
+++ Q8_Array_Repostion_with_Value_in_Position.py
@@ -4,6 +4,16 @@
 
 array = [1, 3, 2, 4, 0, 5]
 
-# cheating way, list expression, haha
+# result preview, list expression, haha
 print([array[x] for x in array])
 
+# smart approach: we need to store the original arr[i] and arr[arr[i]] information in the ithe position with one value
+# and later extract it
+def arg_arr(array, size):
+    for i in range(size):
+        array[i] += size*(array[array[i]]%size)
+    for i in range(size):
+        array[i] = int(array[i]/size)
+    return array
+
+print(arg_arr(array, len(array)))
